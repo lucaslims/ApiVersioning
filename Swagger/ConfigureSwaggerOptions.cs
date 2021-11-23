4ae60513298853f1d71f85e3f7d61ad2312c153c
Lucas Lima <ls_lima@icloud.com>
2021-11-23 15:59:30 -0300

    first commit

---
+++ Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+
+namespace ApiVersioning.Swagger
+{
+    /// <summary> 
+    /// Configura as opções de geração do Swagger.
+    /// </summary>
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider provider;
+
+        /// <summary>
+        /// Inicializa uma nova instância da casse <see cref="ConfigureSwaggerOptions"/>.
+        /// </summary>
+        /// <param name="provider">O <see cref="IApiVersionDescriptionProvider">provedor</see> usado para gerar documentos Swagger.</param>
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+        /// <inheritdoc />
+        public void Configure(SwaggerGenOptions options)
+        {
+            // Adiciona um documento swagger para cada versão de API descoberta
+            foreach (var description in provider.ApiVersionDescriptions)
+            {
+                try
+                {
+                    options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                }
+                catch (Exception) { }
+            }
+        }
+
+        static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo()
+            {
+                Title = "Api",
+                Version = description.ApiVersion.ToString()
+            };
+
+            switch (description.GroupName)
+            {
+                case "v1":
+                    info.Title = "Api";
+                    info.Description = "Web API em ASP.NET Core v5.0";
+                    info.Contact = new OpenApiContact
+                    {
+                        Name = "Lucas Lima",
+                        Email = "ls_lima@icloud.com",
+                        Url = new Uri("https://github.com/lucaslims"),
+                    };
+                    break;
+                case "v2":
+                    info.Title = "Api";
+                    info.Description = "Web API em ASP.NET Core v5.0";
+                    info.Contact = new OpenApiContact
+                    {
+                        Name = "Lucas Lima",
+                        Email = "ls_lima@icloud.com",
+                        Url = new Uri("https://github.com/lucaslims"),
+                    };
+                    break;
+                default:
+                    break;
+            }
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+            return info;
+        }
+    }
+}
---
+++ Swagger/SwaggerDefaultValues.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace ApiVersioning.Swagger
+{
+    /// <summary>
+    /// Configura os valores padrões do Swagger.
+    /// </summary>
+    public class SwaggerDefaultValues : IOperationFilter
+    {
+        /// <summary>
+        /// Aplica o filtro à operação especificada usando o contexto fornecido.
+        /// </summary>
+        /// <param name="operation">A operação à qual aplicar o filtro.</param>
+        /// <param name="context">O contexto do filtro de operação atual.</param>
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var apiDescription = context.ApiDescription;
+            operation.Deprecated |= apiDescription.IsDeprecated();
+
+            if (operation.Parameters == null)
+                return;
+
+            foreach (var parameter in operation.Parameters)
+            {
+                var description = apiDescription.ParameterDescriptions.First(p => p.Name == parameter.Name);
+                if (parameter.Description == null)
+                {
+                    parameter.Description = description.ModelMetadata?.Description;
+                }
+
+                if (parameter.Schema.Default == null && description.DefaultValue != null)
+                {
+                    parameter.Schema.Default = new OpenApiString(description.DefaultValue.ToString());
+                }
+
+                parameter.Required |= description.IsRequired;
+            }
+        }
+    }
+}
